f9ff9b9fd7731fd1af79893071bbe20a3a3c4ed0
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-11-13 11:13:52 -0300

    feat: Adicionado botão personalizado CustomButton e instalação de eact-icons e classnames (Frontend)
    - Implementado a criação de um botão personalizado
    - Instalação da biblioteca 'react-icons' para facilitar a inclusão de ícones
    - Instalação da biblioteca 'classnames' para gerenciar de forma eficiente as classes CSS

---
+++ src/components/CustomButton.js
@@ -0,0 +1,36 @@
+import classNames from "classnames";
+import styles from "@/app/styles/CustomButton.module.css";
+
+const CustomButton = ({
+    buttonType,
+    linkURL,
+    buttonStyle,
+    buttonFunction,
+    buttonIcon,
+    buttonText,
+}) => {
+
+    // Classe condicional com base no botãoStyle
+    const buttonClasses = classNames(
+        styles.customButton, // classe padrão do botão
+        {
+            [styles.orangeButton]: buttonStyle === "orange",
+        }
+    );
+
+    return (
+        <button
+            className={buttonClasses}
+            type={buttonType}
+            href={linkURL}
+            onClick={buttonFunction}
+        >
+            {buttonText}
+            <span>
+                {buttonIcon}
+            </span>
+        </button>
+    );
+};
+
+export default CustomButton;
